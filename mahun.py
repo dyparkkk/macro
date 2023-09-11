baf48be9fb81b1a311390e69763a044b0bdb553c
Park Doyoung <dkdlel1z@naver.com>
2023-09-12 00:53:17 +0900

    시간 선택 추가중

--- mahun.py
+++ mahun.py
@@ -11,6 +11,36 @@ import src.component.login as login
 driver = webdriver.Chrome(service= Service(ChromeDriverManager().install()))
 url = "https://booking.naver.com/booking/10/bizes/210031"
 
+def choice_date(driver, day):
+  driver.implicitly_wait(2)
+  driver.find_element(By.PARTIAL_LINK_TEXT, day).click()
+
+def choice_time(driver, isAm, time, duration):
+  driver.implicitly_wait(2)
+  parent = driver.find_element(By.PARTIAL_LINK_TEXT, '오후')
+  if isAm:
+    parent = driver.find_element(By.PARTIAL_LINK_TEXT, '오전')
+    
+  # 시간 고르기
+  possibleElements = parent.find_elements(By.CLASS_NAME, 'anchor')
+  isCheck = False
+  for element in possibleElements:
+    # if time and time+duration 존재 ?
+    timedElement = element.find_element(By.PARTIAL_LINK_TEXT, time)
+    if timedElement.isDisplayed():
+      timedElement.click()
+      isCheck = True
+      break
+  
+  if isCheck:
+    driver.find_element(By.CLASS_NAME, 'btn_srch on').click()
+    driver.implicity_wait(1)
+    driver.find_element(By.PARTIAL_LINK_TEXT, '다음단계').click()
+    driver.implicity_wait(1)
+    driver.find_element(By.PARTIAL_LINK_TEXT, '결제하기').click()
+
+
+
 def main():
   driver.get(url)
 # xpath = '//태그[@속성="속성값"]'
@@ -19,23 +49,17 @@ def main():
 
   # 코트 선택
   driver.implicitly_wait(2)
-  driver.find_element(By.PARTIAL_LINK_TEXT, '9월 A코트(실내)').click() 
+  elements = driver.find_elements(By.CLASS_NAME, 'lnk_item_book')
+  for element in elements:
+    element.click()
+     # 날짜 선택
+    choice_date(driver, '12')
 
-  # 날짜 선택
-  driver.implicitly_wait(2)
-  day = '12'
-  driver.find_element(By.PARTIAL_LINK_TEXT, day).click()
+    # 시간 선택
+    choice_time(driver, True, '12:00', 1)
+    driver.back()
 
-  # 시간 선택
-  driver.implicitly_wait(2)
-  wanted_times = ['8:00', '9:00', '10:00', '11:00', '12:00']
-  elements =  driver.find_elements(By.CLASS_NAME, 'anchor')
-  print (elements)
-  if len(elements) < 0:
-    print('예약 가능한 시간이 없습니다.')
-
-  # for element in elements:
-  #   if driver.
+  
 
   time.sleep(5)
 
